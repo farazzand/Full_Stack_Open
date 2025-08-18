9f335d1281232855ded302476cff8f41f2ab5d26
Faraz <faraz.zand@gmail.com>
2025-08-18 02:03:42 +0200

    Unicafe final

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+  const StatisticLine = ({text, value, text2}) => {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}{text2}</td>
+      </tr>
+    )
+  }
+
+  const Statistics = ({good, neutral, bad}) => {   
+    const total = good + neutral + bad  
+      if (total === 0) {
+        return(
+          <div>
+          <p><b>statistics</b></p>
+          <div>No feedback given</div>
+          </div>
+        )
+      }
+      const average = (good - bad) / total
+      const positive = (good / total) * 100
+      return(
+        <div>
+          <p><b>statistics</b></p>
+          <table>
+            <tbody>
+              <StatisticLine text="good: " value={good}/>
+              <StatisticLine text="neutral: " value={neutral}/>
+              <StatisticLine text="bad: " value={bad}/>
+              <StatisticLine text="all: " value={total}/>
+              <StatisticLine text="average: " value={average.toFixed(1)}/>
+              <StatisticLine text="positive: " value={positive.toFixed(1)} text2="%"/>
+            </tbody>
+          </table>
+        </div>
+      )
+  }
+  const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+  }
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  console.log('rendering with good value', good)
+  const [neutral, setNeutral] = useState(0)
+  console.log('rendering with neutral value', neutral)
+  const [bad, setBad] = useState(0)
+  console.log('rendering with bad value', bad)
+
+  const goodIncrease = () => {
+    console.log('increasing good, value before', good)
+    setGood(good + 1)
+  }
+
+  const neutralIncrease = () => { 
+    console.log('increasing neutral, value before', neutral)
+    setNeutral(neutral + 1)
+  }
+
+  const badIncrease = () => {
+    console.log('increasing bad, value before', bad)
+    setBad(bad + 1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p><b>give feedback</b></p>
+
+      <Button
+        onClick={goodIncrease}
+        text='good'
+      />
+      <Button
+        onClick={neutralIncrease}
+        text='neutral'
+      />     
+      <Button
+        onClick={badIncrease}
+        text='bad'
+      />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
+    </div>
   )
 }
 
