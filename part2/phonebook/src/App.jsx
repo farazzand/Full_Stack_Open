499ec6360513921ae7ee71b7e7c9beda23715383
Faraz <faraz.zand@gmail.com>
2025-09-22 01:52:44 +0200

    part 2b

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,122 @@
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
+import { useState } from 'react'
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+    return (
+        <div>
+            <form onSubmit={addPerson}>
+                <div>
+                name: <input 
+                    value={newName}
+                    onChange={handleNameChange}
+                />
+                </div>
+                <div>
+                number: <input 
+                    value={newNumber}
+                    onChange={handleNumberChange}
+                />
+                </div>
+                <div>
+                <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const Filter = ({showAll, handleFilterChange}) => {
+    return(
+        <div>
+            filter shown with <input
+            value={showAll}
+            onChange={handleFilterChange}
+            />
+        </div>
+    )
+}
+
+const Persons = ({personsToShow}) => {
+  return(
+    <ul>
+        {personsToShow.map(person => 
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        )}
+      </ul>
   )
 }
 
-const App = ({ notes }) => {
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: newName,
+      id: String(persons.length+1),
+      number: newNumber
+    }
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
+      alert(`${newName} is already added to phonebook`)
+    } else if (newName ==='' || newNumber ===''){
+      alert(`All the fields must be filled`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+    
+  }
+
+  const handleNameChange = (event) => {
+    console.log('new name: ', event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log('new number: ', event.target.value)
+    setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    console.log('new filter: ', event.target.value)
+    setShowAll(event.target.value)
+
+  }
+  
+  const personsToShow = showAll === ''
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(showAll.toLowerCase()))
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <h2>Phonebook</h2>
+      <Filter 
+        showAll = {showAll}
+        handleFilterChange = {handleFilterChange}
+      />
+      <h2>Add a new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h2>Numbers</h2>
+      <Persons 
+      personsToShow={personsToShow}
+      />
     </div>
   )
 }
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,24 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
